fb12a7b076abb8a99f78317d3ec93135b27a8342
Valentin Raduti <raduti@adobe.com>
2021-03-08 10:26:21 +0200

    added facebook login prototype

--- App.tsx
+++ App.tsx
@@ -6,6 +6,21 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import firebase from 'firebase';
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "safe-walk-c2b32.firebaseapp.com",
+    projectId: "safe-walk-c2b32",
+    storageBucket: "safe-walk-c2b32.appspot.com",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import * as Facebook from 'expo-auth-session/providers/facebook';
+import * as WebBrowser from 'expo-web-browser';
+
+import firebase from 'firebase';
+import { ResponseType } from 'expo-auth-session';
+
+var requestInProgress = false;
+
+const permissions = ['public_profile', 'email'];  // Permissions required, consult Facebook docs
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function TabOneScreen() {
+  
+  const [request, response, promptAsync] = Facebook.useAuthRequest({
+      clientId: process.env.FACEBOOK_APP_ID,
+      responseType: ResponseType.Token
+    });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      }
+      requestInProgress = false;
+  }, [response]);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Button disabled ={requestInProgress} onPress={() => {requestInProgress = true;promptAsync();}} title="Login with Facebok">Login</Button>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
